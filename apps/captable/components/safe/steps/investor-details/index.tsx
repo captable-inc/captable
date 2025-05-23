8ae2ebd8639cf0ccc122fbbf40961279077da35e
Puru D <puru@dahal.me>
2025-05-23 02:20:17 -0500

    fix: typecheck

--- apps/captable/components/safe/steps/investor-details/index.tsx
+++ apps/captable/components/safe/steps/investor-details/index.tsx
@@ -2,7 +2,7 @@
 
 import { api } from "@/trpc/server";
 import { InvestorDetailsForm } from "./form";
-export { type TFormSchema } from "./form";
+export type { TFormSchema } from "./form";
 
 export async function InvestorDetails() {
   const stakeholders = await api.stakeholder.getStakeholders.query();
